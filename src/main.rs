a8a6b40e425479ff8793b27f9ffbd784b3147330
trevligttrevande <33224385+trevligttrevande@users.noreply.github.com>
2017-11-03 14:04:31 +0100

    Assignment 2a Done
    The input for the guessing game has been moved into its own function.

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,10 @@ fn main() {
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Bad Input! Very bad!");
+        let guess: u32 = match get_guess(){
+            Ok(num)=> num,
+            Err(e) => {
+                println!("ERROR: {}",e);
                 continue;
             }
         };
@@ -38,5 +33,19 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+// This function handles the input for the guessing game.
+// If the input is not an integer it returns an error instead.
+fn get_guess()->Result<u32, std::num::ParseIntError> 
+{
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+    match guess.trim().parse() {
+        Ok(num)=>Ok(num),
+        Err(e) =>Err(e),
     }
 }
